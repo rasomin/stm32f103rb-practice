7cb75494a1f287ee38cb65bce164bac7cb620edb
Cheolu Choi <ray@rasom.in>
2025-01-17 20:34:14 +0900

    apMain() led 점멸 로직 수정

--- src/ap/ap.c
+++ src/ap/ap.c
@@ -16,9 +16,15 @@ void apInit(void)
 
 void apMain(void)
 {
+	uint32_t pre_time;
+
+	pre_time = millis();
 	while (1)
 	{
-		ledToggle(_DEF_LED1);
-		delay(1500);
+		if (millis() - pre_time >= 500)
+		{
+			pre_time = millis();
+			ledToggle(_DEF_LED1);
+		}
 	}
 }
