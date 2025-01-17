f5b199066c742d223df239a7f9e87d1526b18537
Cheolu Choi <ray@rasom.in>
2025-01-17 22:30:30 +0900

    cdc api 추가

--- src/ap/ap.c
+++ src/ap/ap.c
@@ -9,7 +9,10 @@
 #include "ap.h"
 
 
-extern uint8_t CDC_Transmit_FS(uint8_t* Buf, uint16_t Len);
+extern uint32_t cdcAvailable(void);
+extern uint8_t cdcRead(void);
+extern void cdcDataIn(uint8_t rx_data);
+extern uint32_t cdcWrite(uint8_t *p_data, uint32_t length);
 
 void apInit(void)
 {
@@ -18,11 +21,26 @@ void apInit(void)
 
 void apMain(void)
 {
+	uint32_t pre_time;
+
+	pre_time = millis();
+
 	while (1)
 	{
-		ledToggle(_DEF_LED1);
-		delay(500);
+		if (millis() - pre_time >= 500)
+		{
+			pre_time = millis();
+			ledToggle(_DEF_LED1);
+		}
 
-		CDC_Transmit_FS("test\n", 6);
+		if (cdcAvailable() > 0)
+		{
+			uint8_t rx_data;
+
+			rx_data = cdcRead();
+			cdcWrite((uint8_t *)"RxData : ", 10);
+			cdcWrite(&rx_data, 1);
+			cdcWrite((uint8_t *)"\n", 2);
+		}
 	}
 }
