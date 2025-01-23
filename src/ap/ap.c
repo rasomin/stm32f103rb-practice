c379ea77286b7ee3902d464403650ade381014f8
Cheolu Choi <ray@rasom.in>
2025-01-24 02:30:22 +0900

    cli 구동 코드 추가

--- src/ap/ap.c
+++ src/ap/ap.c
@@ -12,8 +12,11 @@
 
 void apInit(void)
 {
-	uartOpen(_DEF_UART1, 115200); // CDC
+	// uartOpen(_DEF_UART1, 115200); // CDC
 	uartOpen(_DEF_UART2, 115200);
+
+	cliOpen(_DEF_UART2, 115200);
+	// cliOpenLog(_DEF_UART2, 115200);
 }
 
 void apMain(void)
@@ -30,60 +33,15 @@ void apMain(void)
 			ledToggle(_DEF_LED1);
 		}
 
-		if (uartAvailable(_DEF_UART2) > 0)
-		{
-			uint8_t rx_data;
+		// if (uartAvailable(_DEF_UART2) > 0)
+		// {
+		// uint8_t rx_data;
+		// rx_data = uartRead(_DEF_UART2);
 
-			rx_data = uartRead(_DEF_UART2);
+		// uartPrintf(_DEF_UART2, "Rx : 0x%X\n", rx_data);
+		// }
 
-			if (rx_data == '1')
-			{
-				uint8_t buf[32];
-
-				logPrintf("Read...\n");
-
-				flashRead(0x8000000 + (60*1024), buf, 32);
-
-				for (int i = 0; i < 32; i++)
-				{
-					logPrintf("0x%X : 0x%X\n", 0x8000000 + (60*1024)+i, buf[i]);
-				}
-			}
-
-			if (rx_data == '2')
-			{
-				logPrintf("Erase...\n");
-
-				if (flashErase(0x8000000 + (60*1024), 32) == true)
-				{
-					logPrintf("Erase OK\n");
-				}
-				else
-				{
-					logPrintf("Erase Fail\n");
-				}
-			}
-
-			if (rx_data == '3')
-			{
-				uint8_t buf[32];
-
-				for (int i = 0; i < 32; i++)
-				{
-					buf[i] = i;
-				}
-
-				logPrintf("Write...\n");
-
-				if (flashWrite(0x8000000 + (60*1024), buf, 32) == true)
-				{
-					logPrintf("Write OK\n");
-				}
-				else
-				{
-					logPrintf("Write Fail\n");
-				}
-			}
-		}
+		cliMain();
+		
 	}
 }
